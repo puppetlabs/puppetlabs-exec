57eee830dc383c626ce97e20edfce2859a48aa82
Alex Dreyer <alex@puppetlabs.com>
2017-09-12 15:58:57 -0700

    (FM-6361) example output for exec task

---
+++ tasks/init.rb
@@ -0,0 +1,47 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'json'
+require 'open3'
+
+def get3(command)
+  stdout, stderr, status = Open3.capture3(command)
+
+  { stdout: stdout.strip,
+    stderr: stderr.strip,
+    exit_code: status.exitstatus }
+end
+
+def get2(command)
+  output, exit_code  = Open3.popen2({}, command, {:err => [:child, :out]}) do  |i, o, w|
+    out = o.read()
+    exit_code = w.value.exitstatus
+    [out, exit_code]
+  end
+
+  { _output: output,
+    exit_code: exit_code
+  }
+end
+
+def default_true(params, k)
+  v = params[k]
+  if v.nil?
+    true
+  end
+end
+
+params = JSON.parse(STDIN.read)
+command = params['command']
+failonfail = default_true(params, 'failonfail')
+interleave = default_true(params, 'interleave')
+
+begin
+  result = interleave ? get2(command) : get3(command)
+  puts result.to_json
+
+  if failonfail
+    exit result[:exit_code]
+  else
+    exit 0
+  end
+end
