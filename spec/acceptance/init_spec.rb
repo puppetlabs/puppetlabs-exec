0be586588eac3794dda080cc51a2306d8acf696c
tphoney <tp@puppet.com>
2017-09-15 18:40:52 +0100

    remove bolt, use latest spec_helper_acceptance

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper_acceptance'
 describe 'exec task' do
   describe 'puppet exec' do
     it 'single command' do
-      result = run_puppet_task(task_name: 'exec', params: { 'command' => 'uname' })
+      result = run_task(task_name: 'exec', params: 'command=uname')
       expect_multiple_regexes(result: result, regexes: [%r{Linux}, %r{Job completed. 1/1 nodes succeeded}])
     end
     it 'single command with args' do
-      result = run_puppet_task(task_name: 'exec', params: { 'command' => 'df -h' })
+      result = run_task(task_name: 'exec', params: 'command="df -h"')
       expect_multiple_regexes(result: result, regexes: [%r{Filesystem.*Size.*Used.*Avail.*Use}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
