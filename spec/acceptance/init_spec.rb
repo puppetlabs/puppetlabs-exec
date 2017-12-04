179b4658805c9a6d2a87d8994e7cbc040137bf26
Will Meek <william.meek@puppet.com>
2017-12-04 18:03:50 +0000

    Test Cleanup

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,15 +1,28 @@
 # run a test task
 require 'spec_helper_acceptance'
 
+def args_command_line_text
+  command_text = 'command='
+  if fact_on(default, 'osfamily') == 'windows'
+    command_text << "\\"
+  end
+  command_text << "'puppet man module --render-as s"
+  if fact_on(default, 'osfamily') == 'windows'
+    command_text << "\\"
+  end
+  command_text << "'"
+  command_text
+end
+
 describe 'exec task' do
   describe 'puppet exec' do
     it 'single command' do
-      result = run_task(task_name: 'exec', params: 'command=uname')
-      expect_multiple_regexes(result: result, regexes: [%r{Linux}, %r{Job completed. 1/1 nodes succeeded}])
+      result = run_task(task_name: 'exec', params: 'command=netstat')
+      expect_multiple_regexes(result: result, regexes: [%r{Proto.*Local Address.*Foreign Address.*(S|s)tate}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
     end
     it 'single command with args' do
-      result = run_task(task_name: 'exec', params: 'command="df -h"')
-      expect_multiple_regexes(result: result, regexes: [%r{Filesystem.*Size.*Used.*Avail.*Use}, %r{Job completed. 1/1 nodes succeeded}])
+      result = run_task(task_name: 'exec', params: args_command_line_text)
+      expect_multiple_regexes(result: result, regexes: [%r{.*Creates, installs and searches for modules on the Puppet Forge.*}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
     end
   end
 end
