edc4919d50ea99ac9e683d5e216d04928f1b66aa
Michael T Lombardi <michael.t.lombardi@outlook.com>
2019-07-01 11:24:29 -0500

    (FM-8217) Port module to litmus
    This commit updates the acceptance testing of the module
    to use litmus instead of beaker.

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -2,20 +2,11 @@
 require 'spec_helper_acceptance'
 
 describe 'exec task' do
-  include Beaker::TaskHelper::Inventory
-  include BoltSpec::Run
-
   describe 'puppet exec' do
-    it 'single command' do
-      result = task_run('exec', 'command' => 'puppet')
-
-      expect(result.first['result']['exit_code']).to eq 0
-      expect(result.first['result']['_output']).to match %r{See 'puppet help' for help on available puppet subcommands}
-    end
     it 'single command with args' do
-      result = task_run('exec', 'command' => 'puppet help module search')
-      expect(result.first['result']['exit_code']).to eq 0
-      expect(result.first['result']['_output']).to match %r{.*USAGE: puppet module search <search_term>.*}
+      result = run_bolt_task('exec', 'command' => 'echo This is a test')
+      expect(result.exit_code).to eq 0
+      expect(result.stdout).to match %r{This is a test}
     end
   end
 end
--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -2,13 +2,9 @@
 require 'spec_helper_acceptance'
 
 describe 'linux exec task', unless: os[:family] == 'windows' do
-  include Beaker::TaskHelper::Inventory
-  include BoltSpec::Run
-
   it 'echo' do
-    result = task_run('exec::linux', 'command' => 'echo Miku is the best')
-
-    expect(result.first['status']).to eq 'success'
-    expect(result.first['result']['_output']).to match %r{Miku is the best}
+    result = run_bolt_task('exec::linux', 'command' => 'echo Miku is the best')
+    expect(result.exit_code).to eq 0
+    expect(result.stdout).to match %r{Miku is the best}
   end
 end
